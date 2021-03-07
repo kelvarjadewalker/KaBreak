0f0b3d1ac462540edd64e5cd28eafa673f8e9caf
Kelvar Jadewalker <kelvarjadewalker@gmail.com>
2021-03-07 14:12:17 -0500

    started a level manager

---
+++ Assets/Scripts/KelvarJadewalker/KaBreak/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace KelvarJadewalker.KaBreak
+{
+    public class LevelManager : MonoBehaviour
+    {
+        // The Level manager handles game related object for this scene/level
+        private GameManager _gameManager;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            // By convention this is guaranteed to exist
+            _gameManager = FindObjectOfType<GameManager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
